b1a5833a0a52acfe71dfa44e62208e32cad4c943
rfabbri <rfabbri@ubuntu.(none)>
2012-02-09 22:49:02 -0200

    realizado o setup.py minimalista

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name="figgus",
+  version="omega",
+  author_email="renato.fabbri@gmail.com",
+  description="FiGGUS: Finite Groups in Granular and Unit Synthesis",
+  license="GPL",
+  keywords="groups music permutation",
+  url="labmacambira.sf.net",
+  packages=["figgus"],
+)
