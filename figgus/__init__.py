58e488ae1dca2e9b44e06a8b9cc813045c0c6c1a
rfabbri <rfabbri@ubuntu.(none)>
2012-02-09 22:37:21 -0200

    finalizado sobre a escrita em modulo do FIGGUS

---
+++ figgus/__init__.py
@@ -0,0 +1,1 @@
+from FIGGUS import *
--- figgus/tables.py
+++ figgus/tables.py
@@ -1,24 +1,24 @@
 class Tables:
-    f=open("tables/sin1024.txt","r")
+    f=open("figgus/tables/sin1024.txt","r")
     sin1024=[float(i) for i in f.read().split(",")]
     f.close()
     
-    f=open("tables/sin2048.txt","r")
+    f=open("figgus/tables/sin2048.txt","r")
     sin2048=[float(i) for i in f.read().split(",")]
     f.close()
     
-    f=open("tables/saw1024.txt","r")
+    f=open("figgus/tables/saw1024.txt","r")
     saw1024=[float(i) for i in f.read().split(",")]
     f.close()
     
-    f=open("tables/saw2048.txt","r")
+    f=open("figgus/tables/saw2048.txt","r")
     saw2048=[float(i) for i in f.read().split(",")]
     f.close()
     
-    f=open("tables/wnoise1024.txt","r")
+    f=open("figgus/tables/wnoise1024.txt","r")
     wnoise1024=[float(i) for i in f.read().split(",")]
     f.close()
     
-    f=open("tables/wnoise2048.txt","r")
+    f=open("figgus/tables/wnoise2048.txt","r")
     wnoise2048=[float(i) for i in f.read().split(",")]
     f.close()
