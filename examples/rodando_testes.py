30acfe70a06c84427ed878486c8dc4a2a2fdf83b
rfabbri <rfabbri@ubuntu.(none)>
2012-02-09 22:57:26 -0200

    simples modificacao do teste para contemplar a estrutura do modulo

--- examples/rodando_testes.py
+++ examples/rodando_testes.py
@@ -1,9 +1,7 @@
-from FIGGUS import *
-import pylab as P
+from figgus import *
 
 p=Pattern()
 p.synthesizeSonicVectors()
-#print p.sonic_vector #eh pra rolar 3s de senoide a 220Hz, (sao 3 x 1s)
 
 io=IOUtils()
 io.recordPattern("som.wav",p)
