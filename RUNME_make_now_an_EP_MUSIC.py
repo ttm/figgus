d2d6afa602091aa227b096f89fc839695e347949
rfabbri <rfabbri@ubuntu.(none)>
2012-02-12 10:52:58 -0200

    feito arquivo para geracao do EP Solvente

---
+++ RUNME_make_now_an_EP_MUSIC.py
@@ -0,0 +1,18 @@
+import os
+
+#os.mkdir('ep-Projeto_Solvente')
+
+#os.chdir('examples')
+
+
+execfile("examples/try5_B.py")
+os.system('mv som.wav ep-Projeto_Solvente/05-Tiner.wav')
+
+execfile("examples/try6_B.py")
+os.system('mv som.wav ep-Projeto_Solvente/06-Tolueno.wav')
+
+execfile("examples/try7.py")
+os.system('mv som.wav ep-Projeto_Solvente/07-Benzina.wav')
+
+execfile("examples/try7.py")
+os.system('mv som.wav ep-Projeto_Solvente/07-Benzina.wav')
