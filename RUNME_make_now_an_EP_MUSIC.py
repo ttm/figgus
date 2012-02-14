c0c4f3ed20e2986ada3d640fc97577901c478b08
rfabbri <rfabbri@ubuntu.(none)>
2012-02-14 17:07:48 -0200

    fazendo musicas, melhorando o Eter

--- RUNME_make_now_an_EP_MUSIC.py
+++ RUNME_make_now_an_EP_MUSIC.py
@@ -5,30 +5,42 @@ os.mkdir('ep-Projeto_Solvente')
 #os.chdir('examples')
 
 
+execfile("examples/triangulo3_B.py")
+os.system('mv sound.wav ep-Projeto_Solvente/01-Eter.wav')
+print "gravado o arquivo 'ep-Projeto_Solvente/01-Eter.wav'"
+
+#execfile("examples/triangulo3_B.py")
+#os.system('mv sound.wav ep-Projeto_Solvente/02-Alcool.wav')
+#print "gravado o arquivo 'ep-Projeto_Solvente/02-Alcool.wav'"
+
+#execfile("examples/triangulo3_B.py")
+#os.system('mv sound.wav ep-Projeto_Solvente/01-Eter.wav')
+#print "gravado o arquivo 'ep-Projeto_Solvente/01-Eter.wav'"
+
 execfile("examples/try4_B.py")
 os.system('mv sound.wav ep-Projeto_Solvente/04-Butano.wav')
-print "gravado o arquivo ep-Projeto_Solvente/04-Butano.wav"
+print "gravado o arquivo 'ep-Projeto_Solvente/04-Butano.wav'"
 
 execfile("examples/try5_B.py")
-os.system('mv sound.wav ep-Projeto_Solvente/05-Tiner.wav')
-print "gravado o arquivo ep-Projeto_Solvente/05-Tiner.wav"
+os.system('mv sound.wav ep-Projeto_Solvente/05-Thinner.wav')
+print "gravado o arquivo 'ep-Projeto_Solvente/05-Thinner.wav'"
 
 execfile("examples/try6_B.py")
 os.system('mv sound.wav ep-Projeto_Solvente/06-Tolueno.wav')
-print "gravado o arquivo ep-Projeto_Solvente/06-Tolueno.wav"
+print "gravado o arquivo 'ep-Projeto_Solvente/06-Tolueno.wav'"
 
 execfile("examples/try7.py")
 os.system('mv both.wav ep-Projeto_Solvente/07-Benzina.wav')
-print "gravado o arquivo ep-Projeto_Solvente/07-Benzina.wav"
+print "gravado o arquivo 'ep-Projeto_Solvente/07-Benzina.wav'"
 
 execfile("examples/try3_B.py")
 os.system('mv sound.wav ep-Projeto_Solvente/08-LSA.wav')
-print "gravado o arquivo ep-Projeto_Solvente/08-LSA.wav"
+print "gravado o arquivo 'ep-Projeto_Solvente/08-LSA.wav'"
 
 execfile("examples/try2.py")
 os.system('mv sound.wav ep-Projeto_Solvente/09-Cloroformio.wav')
-print "gravado o arquivo ep-Projeto_Solvente/09-Cloroformio.wav"
+print "gravado o arquivo 'ep-Projeto_Solvente/09-Cloroformio.wav'"
 
 execfile("examples/try5.py")
 os.system('mv sound.wav ep-Projeto_Solvente/10-Agua.wav')
-print "gravado o arquivo ep-Projeto_Solvente/10-Agua.wav"
+print "gravado o arquivo 'ep-Projeto_Solvente/10-Agua.wav'"
